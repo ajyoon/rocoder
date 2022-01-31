2ac0d85eba1a0803a0652965a4dc4e26766b6bdd
Andrew Yoon <andrew@nothing-to-say.org>
2022-01-30 21:12:59 -0500

    remove old cruft

--- hotswap_sample.rs
+++
@@ -1,4 +0,0 @@
-#[no_mangle]
-pub fn test(input: Vec<f32>) -> Vec<f32> {
-    return input.iter().map(|x| x * 3.0).collect();
-}
--- sketch.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-for i in range(9):
-    subprocess.call(['cargo', 'run', '--release', '--', 
-                     'bach_kyrie.wav', f'out_{i}.wav', 
-                     '-f', '3', '-w', str(2 ** (15 - i))])
