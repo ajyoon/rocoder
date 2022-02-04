791adcebafa1975e9e468df9604f8f40d68601f7
Andrew Yoon <andrew@nothing-to-say.org>
2022-02-04 16:27:08 -0500

    Move remaining installation code to other repo

--- installation_scripts/hours_manager.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import subprocess
-
-hour = datetime.now().hour
-
-if hour < 8 or hour > 20:
-    subprocess.run(['systemctl', 'stop', 'installation'])
-else:
-    subprocess.run(['systemctl', 'start', 'installation', '--now'])
